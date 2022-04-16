ad92f648d500a0731634f8c1ce0be80a836fdf40
ijoybee <60688732+ijoybee@users.noreply.github.com>
2022-04-16 20:40:28 +0800

    Update jegotrip-ooxx.js

--- jegotrip/jegotrip-ooxx.js
+++ jegotrip/jegotrip-ooxx.js
@@ -69,7 +69,7 @@ const headers = {
     'Accept-Language': 'en-us',
     'Referer': '\u0068\u0074\u0074\u0070\u003a\u002f\u002f\u0074\u0061\u0073\u006b\u002e\u006a\u0065\u0067\u006f\u0074\u0072\u0069\u0070\u002e\u0063\u006f\u006d\u002e\u0063\u006e\u003a\u0038\u0030\u0038\u0030\u002f\u0074\u0061\u0073\u006b\u002f\u0069\u006e\u0064\u0065\u0078\u002e\u0068\u0074\u006d\u006c'
 };
-// $.delete('AsVow'); //取消本条注释删除所有Cookie
+ $.delete('AsVow'); //取消本条注释删除所有Cookie
 var AsVow = isNode ? process.env.AsVow : $.read('AsVow');
 var info = '';
 
